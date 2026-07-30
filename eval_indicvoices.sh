8e7c042e989d5ace64de182b8cbe3627f4a67421
bgiddwani-ai <bgiddwani@gmail.com>
2026-07-30 16:46:09 +0530

    Update eval_indicvoices.sh

--- eval_indicvoices.sh
+++ eval_indicvoices.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1 python NeMo/examples/asr/transcribe_speech_parallel.py \
-    model='./models/ckpts/commotion/run1/epoch5/parakeet-multilingual_epoch5.nemo' \
+CUDA_VISIBLE_DEVICES=1 python Speech/examples/asr/transcribe_speech_parallel.py \
+    model='/data/results/indicvoices/new_final_model.nemo' \
     predict_ds.manifest_filepath='dataset/indicvoices/valid/hi_manifest.json' \
     predict_ds.batch_size=32 \
-    output_path="results/indivoices/commotion_run1_epoch5/hi"
+    output_path="/data/results/indivoices/commotion_run1_epoch5/hi"
