ab246f8765ac2c6155738f05545aa5e21b1fc12f
bgiddwani-ai <bgiddwani@gmail.com>
2026-08-17 22:53:52 +0530

    Delete eval_indicvoices.sh

--- eval_indicvoices.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=1 python Speech/examples/asr/transcribe_speech_parallel.py \
-    model='/data/results/indicvoices/new_final_model.nemo' \
-    predict_ds.manifest_filepath='dataset/indicvoices/valid/hi_manifest.json' \
-    predict_ds.batch_size=32 \
-    output_path="/data/results/indivoices/commotion_run1_epoch5/hi"
